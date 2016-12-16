9ec840f6fa206c6053478874eadb3c9b06aa18fb
u0958680 <u0958680@1120-mac-39.lib.utah.edu>
2016-12-16 08:03:55 -0700

    set up

---
+++ Disaster Proto/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int speed;
+	public int hop;
+	public int skip;
+
+	public bool isGrounded = false;
+	public bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
