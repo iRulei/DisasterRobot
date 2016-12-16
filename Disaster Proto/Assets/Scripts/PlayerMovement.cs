a2a5ec2d5fd207f4fda69c9c030fe9162d4a9df0
iRulei <rule.leon@hotmail.com>
2016-12-16 14:56:04 -0700

    implemented Hop() and Skip()

--- Disaster Proto/Assets/Scripts/PlayerMovement.cs
+++ Disaster Proto/Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,19 @@ using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
+	public float accel;
 	public float hopStrength;
+	public float hopBuild;
 	public float skipStrength;
-	private float hopBuild;
-	private float skipBuild;
+	public float skipBuild;
 
 	private bool isGrounded = false;
 	private bool isAlive = true;
-	private Vector3 heading;
+
+	private float hopPow;
+	private float skipPow;
+
+	private Vector3 turnStrength;
 
 //	private int sceneNum;
 
@@ -28,8 +33,8 @@ public class PlayerMovement : MonoBehaviour {
 
 		// initializes the robot's velocity and heading
 		body = GetComponent<Rigidbody> ();
-		body.velocity = new Vector3 (0, 0, 0);
-		heading = transform.forward;
+		body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+		turnStrength = new Vector3 (0, 0.75f, 0);
 	}
 	
 	// Update is called once per frame
@@ -45,26 +50,68 @@ public class PlayerMovement : MonoBehaviour {
 			body.velocity *= 0;
 		} else {
 
-			body.rotation = new Quaternion (0, 0, 0, 0);
+			// accelerating forward
+			if (Input.GetKey (KeyCode.W)) {
+				body.AddRelativeForce (Vector3.forward * accel);
+			} else if (Input.GetKeyUp (KeyCode.W)) {
 
+			} else if (Input.GetKey (KeyCode.S)) {
+				body.AddRelativeForce (Vector3.back * accel);
+			} else if (Input.GetKeyUp (KeyCode.S)) {
+
+			}
+
+			// turning left and right
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Rotate (-turnStrength, Space.Self);
+			} else if (Input.GetKey (KeyCode.D)) {
+				transform.Rotate (turnStrength, Space.Self);
+			}
+
+			// hopping
 			if (Input.GetKey (KeyCode.Space) && isGrounded) {
-				hopBuild += 0.01f;
+				if (hopPow >= hopStrength) {
+					hopPow = hopStrength;
+				} else {
+					hopPow += 0.01f * hopBuild;
+				}
 			} else if (Input.GetKeyUp (KeyCode.Space)) {
 				Hop ();
-				hopBuild = 0f;
+				hopPow = 0f;
 			}
 
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				body.velocity = new Vector3 (0, 0, speed);
-			} else if (Input.GetKeyUp (KeyCode.UpArrow)) {
-				body.velocity = new Vector3 (0, 0, 0);
+			// skipping
+			if (Input.GetKey (KeyCode.LeftShift) && isGrounded) {
+				if (skipPow >= skipStrength) {
+					skipPow = skipStrength;
+				} else {
+					skipPow += 0.01f * skipBuild;
+				}
+			} else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+				Skip ();
+				skipPow = 0f;
 			}
+		}
+	} 
+
+	void Accelerate(bool isForward) {
+		if (isGrounded) {
+			transform.Translate (Vector3.forward * speed);
 		}
 	}
 
 	void Hop() {
 		if (isGrounded) {
-			body.AddForce (Vector3.up * hopStrength * hopBuild);
+			Debug.Log ("hopping: " + hopPow);
+			body.AddForce (Vector3.up * hopPow * 1000);
+			isGrounded = false;
+		}
+	}
+
+	void Skip() {
+		if (isGrounded) {
+			Debug.Log ("skipping: " + skipPow);
+			body.AddRelativeForce (new Vector3(0, skipPow * 500, skipPow * 1000));
 			isGrounded = false;
 		}
 	}
