6c9e36186c7083d150d426ad59849fd3507afea2
iRulei <rule.leon@hotmail.com>
2016-12-20 10:27:54 -0700

    implemented an IsGrounded() method that checks with a raycast when needed (rather than depending on logic and collisions to determine the isGrounded boolean).

--- Disaster Proto/Assets/Scripts/PlayerMovement.cs
+++ Disaster Proto/Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,8 @@ public class PlayerMovement : MonoBehaviour {
 	private float hopPow;
 	private float skipPow;
 	private Vector3 turnVector;
+	private float groundRay;
+	BoxCollider robotCollider;
 
 //	private int sceneNum;
 
@@ -35,6 +37,8 @@ public class PlayerMovement : MonoBehaviour {
 		// initializes the robot
 		body = GetComponent<Rigidbody> ();
 		body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+		robotCollider = gameObject.GetComponent<BoxCollider> ();
+		groundRay = robotCollider.bounds.extents.y;
 		turnVector = new Vector3 (0, (float)(0.20 * efficiency + 0.70), 0);
 		fuel = fuelCapacity * 500;
 	}
@@ -49,11 +53,12 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		//Debug.Log(body.velocity.z + "m/s");
+		Debug.Log(IsGrounded());
 
 		// only move if the robot is alive
 		if (isAlive) {
 
-			if (!isGrounded) {
+			if (!IsGrounded()) {
 				if (body.velocity.z > 0) {
 					body.AddRelativeForce (Vector3.back * 10);
 				} else if (body.velocity.z < 0) {
@@ -62,22 +67,22 @@ public class PlayerMovement : MonoBehaviour {
 			}
 
 			// use W and S to move forward and back, limited by maximum speed
-			if (Input.GetKey (KeyCode.W) && isGrounded && body.velocity.magnitude < (speed + 4)) {
+			if (Input.GetKey (KeyCode.W) && IsGrounded() && body.velocity.magnitude < (speed + 4)) {
 				body.AddRelativeForce (Vector3.forward * (5 * efficiency + 150));
-			} else if (Input.GetKey (KeyCode.S) && isGrounded && body.velocity.magnitude < (speed + 2)) {
+			} else if (Input.GetKey (KeyCode.S) && IsGrounded() && body.velocity.magnitude < (speed + 2)) {
 				body.AddRelativeForce (Vector3.back * (5 * efficiency + 150));
 			}
 
 
 			// TURNING LEFT and RIGHT
 			if (Input.GetKey (KeyCode.A)) {
-				if (isGrounded) {
+				if (IsGrounded()) {
 					transform.Rotate (-turnVector, Space.Self);
 				} else {
 					body.AddRelativeTorque (0, -((5 + thrust) / 20), 0);
 				}
 			} else if (Input.GetKey (KeyCode.D)) {
-				if (isGrounded) {
+				if (IsGrounded()) {
 					transform.Rotate (turnVector, Space.Self);
 				} else {
 					body.AddRelativeTorque (0, ((5 + thrust) / 20), 0);
@@ -88,7 +93,7 @@ public class PlayerMovement : MonoBehaviour {
 			// HOPPING and VENTRAL THRUSTERS
 			if (Input.GetKey (KeyCode.Space)) {
 				// build up / execute a hop if the robot is grounded and thrusters are NOT engaged
-				if (isGrounded && !Input.GetKey (KeyCode.LeftControl)) {
+				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl)) {
 					// grow hopping power only if it's not maxed out
 					if (hopPow >= hop) {
 						hopPow = hop;
@@ -100,12 +105,12 @@ public class PlayerMovement : MonoBehaviour {
 				} else if (Input.GetKey (KeyCode.LeftControl) && fuel > 0 && body.velocity.y < (float)(thrust)) {
 					body.AddRelativeForce (Vector3.up * (int)(2.5 * thrust + 150));
 					fuel -= 2;
-					isGrounded = false;
+					//isGrounded = false;
 					//Debug.Log (100 * fuel / (fuelCapacity * 500) + "% (ventral thrusters at " + (int)(2.5 * thrust + 150) + ")");
 				}
 			// execute a hop when the SPACE key is released
 			} else if (Input.GetKeyUp (KeyCode.Space) && !Input.GetKey (KeyCode.LeftControl)) {
-				if (isGrounded) {
+				if (IsGrounded()) {
 					Hop ();
 				}
 				hopPow = 0f;
@@ -115,17 +120,17 @@ public class PlayerMovement : MonoBehaviour {
 			// SKIPPING and ANTERIOR THRUSTERS
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				// build up / execute a skip if the robot is grounded and thrusters are NOT engaged
-				if (isGrounded && !Input.GetKey (KeyCode.LeftControl)) {
+				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl)) {
 					// grow skipping power only if it's not maxed out
 					if (skipPow >= skip) {
 						skipPow = skip;
 					} else {
-						skipPow += (0.005f * efficiency) + 0.005f;
+						skipPow += skipPow + 0.001f;
 					}
 					Debug.Log ("skip: " + skipPow);
 				// fire anterior thrusters if they are engaged with L-CTRL
 				} else if (Input.GetKey (KeyCode.LeftControl) && fuel > 0 && body.velocity.z < (float)(thrust)) {
-					if (isGrounded) {
+					if (IsGrounded()) {
 						body.AddRelativeForce (Vector3.forward * (int)(5 * thrust + 150));
 					} else {
 						body.AddRelativeForce (Vector3.forward * (int)(5 * thrust + 25));
@@ -135,7 +140,7 @@ public class PlayerMovement : MonoBehaviour {
 				}
 			// execute a skip when the L-SHIFT key is released
 			} else if (Input.GetKeyUp (KeyCode.LeftShift) && !Input.GetKey (KeyCode.LeftControl)) {
-				if (isGrounded) {
+				if (IsGrounded()) {
 					Skip ();
 				}
 				skipPow = 0f;
@@ -147,17 +152,21 @@ public class PlayerMovement : MonoBehaviour {
 	void Hop() {
 		Debug.Log ("Hopping (" + (400 * hopPow + 2400) + ")");
 		body.AddForce (Vector3.up * (400 * hopPow + 2400));
-		isGrounded = false;
+		//isGrounded = false;
 	}
 
 	// launches the robot forward in an arc
 	void Skip() {
-		Debug.Log ("Skipping (" + (500 * skipPow + 3000) + ")");
-		body.AddRelativeForce (new Vector3(0, (250 * skipPow + 1500), (500 * skipPow + 3000)));
-		isGrounded = false;
+		Debug.Log ("Skipping (" + (1000 * skipPow + 500) + ")");
+		body.AddRelativeForce (new Vector3(0, (400 * skipPow + 200), (1000 * skipPow + 500)));
+		//isGrounded = false;
 	}
 
-	void OnCollisionEnter(Collision col) {
-		if (col.gameObject.tag == "Ground") { isGrounded = true; }
+	bool IsGrounded() {
+		return Physics.Raycast (transform.position, Vector3.down, (float)(groundRay + 0.1f));
 	}
+
+//	void OnCollisionEnter(Collision col) {
+//		if (col.gameObject.tag == "Ground") { isGrounded = true; }
+//	}
 }
