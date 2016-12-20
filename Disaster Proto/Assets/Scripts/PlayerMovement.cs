81e7f0fc07c8f6f9e4fd99c8f3369d3f68c2d389
iRulei <rule.leon@hotmail.com>
2016-12-20 14:21:15 -0700

    implemented a RobotAbility class containing a name and cooldown timer.

--- Disaster Proto/Assets/Scripts/PlayerMovement.cs
+++ Disaster Proto/Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,32 @@
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
 	public float thrust;
-	public float hop;
-	public float skip;
+	public float jump;
 	public float efficiency;
-
 	public int fuelCapacity;
-	private int fuel;
-
-	private bool isGrounded = false;
-	private bool isAlive = true;
 
 	private float hopPow;
 	private float skipPow;
-	private Vector3 turnVector;
-	private float groundRay;
-	BoxCollider robotCollider;
+	private int fuel;
 
-//	private int sceneNum;
+	// COOLDOWNS
+	private Dictionary<string, RobotAbility> ableDict;
+
+	private bool isAlive = true;
 
 	private Rigidbody body;
+	BoxCollider robotCollider;
+	private Vector3 turnVector;
+	private float groundRay;
+
+//	private int sceneNum;
 
 	// Use this for initialization
 	void Start () {
@@ -40,6 +41,12 @@ public class PlayerMovement : MonoBehaviour {
 		robotCollider = gameObject.GetComponent<BoxCollider> ();
 		groundRay = robotCollider.bounds.extents.y;
 		turnVector = new Vector3 (0, (float)(0.20 * efficiency + 0.70), 0);
+
+		// sets up the robot's active parameters
+		ableDict = new Dictionary<string, RobotAbility>();
+		RegisterAbilities ();
+		hopPow = 0;
+		skipPow = 0;
 		fuel = fuelCapacity * 500;
 	}
 	
@@ -52,8 +59,10 @@ public class PlayerMovement : MonoBehaviour {
 			isAlive = false;
 		}
 
+		// Useful debugging output about the robot's status
 		//Debug.Log(body.velocity.z + "m/s");
-		Debug.Log(IsGrounded());
+		//Debug.Log(IsGrounded());
+		Debug.Log("hopCD\t" + ableDict["hop"].IsReady + "\nskipCD\t" + ableDict["skip"].IsReady);
 
 		// only move if the robot is alive
 		if (isAlive) {
@@ -62,7 +71,7 @@ public class PlayerMovement : MonoBehaviour {
 				if (body.velocity.z > 0) {
 					body.AddRelativeForce (Vector3.back * 10);
 				} else if (body.velocity.z < 0) {
-					body.AddRelativeForce (Vector3.forward * 2);
+					body.AddRelativeForce (Vector3.forward * 10);
 				}
 			}
 
@@ -93,24 +102,23 @@ public class PlayerMovement : MonoBehaviour {
 			// HOPPING and VENTRAL THRUSTERS
 			if (Input.GetKey (KeyCode.Space)) {
 				// build up / execute a hop if the robot is grounded and thrusters are NOT engaged
-				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl)) {
+				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl) && ableDict["hop"].IsReady) {
 					// grow hopping power only if it's not maxed out
-					if (hopPow >= hop) {
-						hopPow = hop;
+					if (hopPow >= jump) {
+						hopPow = jump;
 					} else {
 						hopPow += (0.01f * efficiency) + 0.01f;
 					}
-					Debug.Log ("hop: " + hopPow);
+					Debug.Log ("building hop...\t" + hopPow);
 				// fire ventral thrusters if they are engaged with L-CTRL
 				} else if (Input.GetKey (KeyCode.LeftControl) && fuel > 0 && body.velocity.y < (float)(thrust)) {
 					body.AddRelativeForce (Vector3.up * (int)(2.5 * thrust + 150));
 					fuel -= 2;
-					//isGrounded = false;
 					//Debug.Log (100 * fuel / (fuelCapacity * 500) + "% (ventral thrusters at " + (int)(2.5 * thrust + 150) + ")");
 				}
 			// execute a hop when the SPACE key is released
 			} else if (Input.GetKeyUp (KeyCode.Space) && !Input.GetKey (KeyCode.LeftControl)) {
-				if (IsGrounded()) {
+				if (IsGrounded() && ableDict["hop"].IsReady) {
 					Hop ();
 				}
 				hopPow = 0f;
@@ -120,14 +128,14 @@ public class PlayerMovement : MonoBehaviour {
 			// SKIPPING and ANTERIOR THRUSTERS
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				// build up / execute a skip if the robot is grounded and thrusters are NOT engaged
-				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl)) {
+				if (IsGrounded() && !Input.GetKey (KeyCode.LeftControl) && ableDict["skip"].IsReady) {
 					// grow skipping power only if it's not maxed out
-					if (skipPow >= skip) {
-						skipPow = skip;
+					if (skipPow >= jump) {
+						skipPow = jump;
 					} else {
-						skipPow += skipPow + 0.001f;
+						skipPow += (0.0005f * efficiency) + (0.025f * skipPow);
 					}
-					Debug.Log ("skip: " + skipPow);
+					Debug.Log ("building skip...\t" + skipPow);
 				// fire anterior thrusters if they are engaged with L-CTRL
 				} else if (Input.GetKey (KeyCode.LeftControl) && fuel > 0 && body.velocity.z < (float)(thrust)) {
 					if (IsGrounded()) {
@@ -140,7 +148,7 @@ public class PlayerMovement : MonoBehaviour {
 				}
 			// execute a skip when the L-SHIFT key is released
 			} else if (Input.GetKeyUp (KeyCode.LeftShift) && !Input.GetKey (KeyCode.LeftControl)) {
-				if (IsGrounded()) {
+				if (IsGrounded() && ableDict["skip"].IsReady) {
 					Skip ();
 				}
 				skipPow = 0f;
@@ -149,17 +157,17 @@ public class PlayerMovement : MonoBehaviour {
 	} 
 
 	// launches the robot straight into the air
-	void Hop() {
+	private void Hop() {
 		Debug.Log ("Hopping (" + (400 * hopPow + 2400) + ")");
 		body.AddForce (Vector3.up * (400 * hopPow + 2400));
-		//isGrounded = false;
+		ableDict["hop"].ActivateCoolDown();
 	}
 
 	// launches the robot forward in an arc
-	void Skip() {
-		Debug.Log ("Skipping (" + (1000 * skipPow + 500) + ")");
-		body.AddRelativeForce (new Vector3(0, (400 * skipPow + 200), (1000 * skipPow + 500)));
-		//isGrounded = false;
+	private void Skip() {
+		Debug.Log ("Skipping (" + (1200 * skipPow + 2000) + "Z, " + (240 * skipPow + 400) + "Y)");
+		body.AddRelativeForce (new Vector3(0, (240 * skipPow + 400), (1200 * skipPow + 2000)));
+		ableDict["skip"].ActivateCoolDown();
 	}
 
 	bool IsGrounded() {
@@ -169,4 +177,48 @@ public class PlayerMovement : MonoBehaviour {
 //	void OnCollisionEnter(Collision col) {
 //		if (col.gameObject.tag == "Ground") { isGrounded = true; }
 //	}
+
+	private void RegisterAbilities () {
+		Debug.Log ("registering abilities");
+		ableDict.Add ("hop", new RobotAbility("hop", (3250 - efficiency * 250)));
+		ableDict.Add ("skip", new RobotAbility ("skip", (3250 - efficiency * 250)));
+	}
+
+	/// <summary>
+	/// A wrapper class for a robot ability, including a string name, a cooldown, and a boolean indicating whether the ability's has recovered from cooldown.
+	/// </summary>
+	protected class RobotAbility {
+
+		private string name;
+		private double interval;
+		private Timer timer;
+		private bool isReady;
+
+		public string Name { get { return name; } }
+		public bool IsReady { get { return isReady; } }
+
+		/// <summary>
+		/// Creates a new RobotAbility with the specified name and cooldown.
+		/// </summary>
+		/// <param name="inName">The name of the ability.</param>
+		/// <param name="timerMS">The cooldown timer in milliseconds.</param>
+		public RobotAbility(string inName, double cooldown) {
+			name = inName;
+			interval = cooldown;
+			timer = new Timer (interval);
+			timer.Elapsed += ReadyAbility;
+			isReady = true;
+		}
+
+		public void ActivateCoolDown() {
+			isReady = false;
+			timer.Start ();
+		}
+
+		public void ReadyAbility(object sender, ElapsedEventArgs e) {
+			isReady = true;
+			timer = new Timer (interval);
+			timer.Elapsed += ReadyAbility;
+		}
+	}
 }
